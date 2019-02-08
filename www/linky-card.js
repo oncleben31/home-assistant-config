75faa751d47901e98e439e45051b4f0dd2517ccf
oncleben31 <oncleben31@users.noreply.github.com>
2019-02-08 11:52:18 +0100

    New custom card for Linky
    To resolve bug when Linky website down

--- www/linky-card.js
+++ www/linky-card.js
@@ -1,25 +1,36 @@
 //import 'https://unpkg.com/@google-web-components/google-chart/google-chart.js?module';
 
 class LinkyCard extends HTMLElement {
-    set hass(hass) {
-      if (!this.content) {
-        const card = document.createElement('ha-card');
-        const link = document.createElement('link');
-        link.type = 'text/css';
-        link.rel = 'stylesheet';
-        link.href = '/local/linky-card.css?v=31';
-        card.appendChild(link);
-        this.content = document.createElement('div');
-        this.content.className = 'card';
-        card.appendChild(this.content);
-        this.appendChild(card);
+  set hass(hass) {
+    const entityId = this.config.entity;
+    const state = hass.states[entityId];
+    const stateStr = state ? state.state : 'unavailable';
+    const attributes = state.attributes;
+    
+    if (!this.content) {
+      const card = document.createElement('ha-card');
+      const link = document.createElement('link');
+      link.type = 'text/css';
+      link.rel = 'stylesheet';
+      link.href = '/local/custom_ui/linky-card.css?v=31';
+      card.appendChild(link);
+      this.content = document.createElement('div');
+      this.content.className = 'card';
+      card.appendChild(this.content);
+      if (stateStr == 'unavailable') {
+          this.content.innerHTML = `
+            <div id="states">
+            <div class="name">
+              <ha-icon id="icon" icon="mdi:flash" data-state="unavailable" data-domain="connection" style="color: var(--state-icon-unavailable-color)"></ha-icon>
+              <span style="margin-right:2em">Linky : Site Enedis.fr inaccessible</span>
+            </div>
+            </div>`
       }
-      
-      const entityId = this.config.entity;
-      const state = hass.states[entityId];
-      const stateStr = state ? state.state : 'unavailable';
-      const attributes = state.attributes;
-      
+      this.appendChild(card);
+    }
+    
+
+    if (stateStr != 'unavailable') {
       this.content.innerHTML = `
         <div class="hp-hc-block">
           <google-chart
@@ -44,26 +55,27 @@ class LinkyCard extends HTMLElement {
           </ul>
         </span>
         <div class="week-history clear">
-            ${Object.keys(attributes.daily.slice(2, 7)).map((day, index) => `
+            ${Object.keys(attributes.daily.slice(2, 7).reverse()).map((day, index) => `
             <div class="day">
-                <span class="dayname">${new Date((new Date().getTime()) - day*60*60*24*1000).toLocaleDateString('fr-FR', {weekday: "long"}).split(' ')[0]}</span>
-                <br><span class="cons-val">${Number.parseFloat(attributes.daily[day]).toFixed(1)} ${attributes.unit_of_measurement}</span>
+                <span class="dayname">${new Date(new Date().setDate(new Date().getDate()-(6-Number.parseInt(day)))).toLocaleDateString('fr-FR', {weekday: "long"}).split(' ')[0]}</span>
+                <br><span class="cons-val">${Number.parseFloat(attributes.daily.slice(2, 7).reverse()[day]).toFixed(1)} ${attributes.unit_of_measurement}</span>
             </div>`).join('')}
         </div>`;
     }
-  
-    setConfig(config) {
-      if (!config.entity) {
-        throw new Error('You need to define an entity');
-      }
-      this.config = config;
-    }
-  
-    // @TODO: This requires more intelligent logic
-    getCardSize() {
-      return 3;
-    }
   }
-  
-  customElements.define('linky-card', LinkyCard);
+
+  setConfig(config) {
+    if (!config.entity) {
+      throw new Error('You need to define an entity');
+    }
+    this.config = config;
+  }
+
+  // @TODO: This requires more intelligent logic
+  getCardSize() {
+    return 3;
+  }
+}
+
+customElements.define('linky-card', LinkyCard);
 
